465bcef0456b3ed1beb2bb45b6b27b4539bf7d7e
dshbwlto <pashbymorrisseau@gmail.com>
2026-03-26 15:52:48 -0400

    Anole item rendering/model

--- src/main/java/net/dshbwlto/createbionics/item/custom/AnoleItemRenderer.java
+++ src/main/java/net/dshbwlto/createbionics/item/custom/AnoleItemRenderer.java
@@ -27,30 +27,32 @@ public class AnoleItemRenderer extends CustomRenderedItemModelRenderer {
         float headYaw = Mth.sin(Mth.cos((AnimationTickHolder.getPartialTicks() + AnimationTickHolder.getTicks()) / 100f) * 1.6f);
         float tailYaw = Mth.sin((AnimationTickHolder.getTicks() + AnimationTickHolder.getPartialTicks()) / 20) / 10;
 
-        ms.mulPose(Axis.XP.rotation(Mth.PI / 9));
-        ms.mulPose(Axis.YP.rotation(tailYaw));
-        renderer.render(TAIL1.get(), light);
-        ms.translate(0,0,5/16f);
-        ms.mulPose(Axis.XP.rotation(Mth.PI / 9));
-        ms.mulPose(Axis.YP.rotation(tailYaw));
-        renderer.render(TAIL2.get(), light);
-        ms.mulPose(Axis.YP.rotation(-tailYaw));
-        ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
-        ms.translate(0,0,-5/16f);
-        ms.mulPose(Axis.YP.rotation(-tailYaw));
-        ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
+        if (transformType == ItemDisplayContext.THIRD_PERSON_LEFT_HAND || transformType == ItemDisplayContext.THIRD_PERSON_RIGHT_HAND) {
 
-        ms.translate(0,0,-3/16f);
-        ms.mulPose(Axis.XP.rotation(Mth.PI/-9));
-        renderer.render(CHEST.get(), light);
-        ms.translate(0,0,-3/16f);
-        ms.mulPose(Axis.XP.rotation(Mth.PI/-9));
-        ms.mulPose(Axis.YP.rotation(headYaw / 5));
-        renderer.render(NECK.get(), light);
-        ms.translate(0,0,-1/8f);
-        ms.mulPose(Axis.XP.rotation(Mth.PI/-18));
-        ms.mulPose(Axis.YP.rotation(headYaw));
-        renderer.render(HEAD.get(), light);
+            ms.mulPose(Axis.XP.rotation(Mth.PI / 9));
+            ms.mulPose(Axis.YP.rotation(tailYaw));
+            renderer.render(TAIL1.get(), light);
+            ms.translate(0, 0, 5 / 16f);
+            ms.mulPose(Axis.XP.rotation(Mth.PI / 9));
+            ms.mulPose(Axis.YP.rotation(tailYaw));
+            renderer.render(TAIL2.get(), light);
+            ms.mulPose(Axis.YP.rotation(-tailYaw));
+            ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
+            ms.translate(0, 0, -5 / 16f);
+            ms.mulPose(Axis.YP.rotation(-tailYaw));
+            ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
 
+            ms.translate(0, 0, -3 / 16f);
+            ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
+            renderer.render(CHEST.get(), light);
+            ms.translate(0, 0, -3 / 16f);
+            ms.mulPose(Axis.XP.rotation(Mth.PI / -9));
+            ms.mulPose(Axis.YP.rotation(headYaw / 5));
+            renderer.render(NECK.get(), light);
+            ms.translate(0, 0, -1 / 8f);
+            ms.mulPose(Axis.XP.rotation(Mth.PI / -18));
+            ms.mulPose(Axis.YP.rotation(headYaw));
+            renderer.render(HEAD.get(), light);
+        }
     }
 }
