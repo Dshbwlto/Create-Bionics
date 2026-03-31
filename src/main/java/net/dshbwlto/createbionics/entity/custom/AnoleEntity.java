b00545bef50f4906598cfe4c47eb4fad30259bce
dshbwlto <pashbymorrisseau@gmail.com>
2026-03-31 17:24:01 -0400

    Anole scares spiders, cave spiders, silverfish, and bees

--- src/main/java/net/dshbwlto/createbionics/entity/custom/AnoleEntity.java
+++ src/main/java/net/dshbwlto/createbionics/entity/custom/AnoleEntity.java
@@ -27,7 +27,10 @@ import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.navigation.WallClimberNavigation;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.animal.Ocelot;
+import net.minecraft.world.entity.monster.CaveSpider;
+import net.minecraft.world.entity.monster.Silverfish;
 import net.minecraft.world.entity.monster.Spider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -109,6 +112,15 @@ public class AnoleEntity extends AbstractRobot {
     public static void scareEntity(EntityJoinLevelEvent event) {
         if (event.getEntity() instanceof Spider spider) {
             spider.goalSelector.addGoal(1, new AvoidEntityGoal(spider, AnoleEntity.class, 6.0F, (double)1.0F, 1.2));;
+        }
+        if (event.getEntity() instanceof CaveSpider caveSpider) {
+            caveSpider.goalSelector.addGoal(1, new AvoidEntityGoal(caveSpider, AnoleEntity.class, 6.0F, (double)1.0F, 1.2));;
+        }
+        if (event.getEntity() instanceof Silverfish silverfish) {
+            silverfish.goalSelector.addGoal(1, new AvoidEntityGoal(silverfish, AnoleEntity.class, 6.0F, (double)1.0F, 1.2));;
+        }
+        if (event.getEntity() instanceof Bee bee) {
+            bee.goalSelector.addGoal(1, new AvoidEntityGoal(bee, AnoleEntity.class, 6.0F, (double)1.0F, 1.2));;
         }
     }
 
