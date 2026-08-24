c705f8e80ac6ecfe211afb807fafa9651f7b348c
dshbwlto <pashbymorrisseau@gmail.com>
2026-08-24 16:34:17 -0400

    Who even looks at these?

--- src/main/java/net/dshbwlto/createbionics/entity/custom/OrganEntity.java
+++ src/main/java/net/dshbwlto/createbionics/entity/custom/OrganEntity.java
@@ -305,7 +305,7 @@ public class OrganEntity extends MultiPartRobot<RobotPartEntity> {
     protected void defineSynchedData(SynchedEntityData.Builder builder) {
         super.defineSynchedData(builder);
         ///   Not yet, hotshot
-        builder.define(GLOW_COLOR, 0);
+        //builder.define(GLOW_COLOR, 0);
     }
 
     @Override
